a828a4def2a84728ad4d885e60dc2cdf0c8bdb20
Luiz Vid <luizvid@gmail.com>
2015-04-06 01:06:35 -0300

    finalizada normalizacao da rota marketing

--- module/Market/config/module.config.php
+++ module/Market/config/module.config.php
@@ -14,148 +14,74 @@ return array(
     ),
     'router' => array(
         'routes' => array(
+            'home' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/',
+                    'defaults' => array(
+                        'controller'    => 'market-index-controller',
+                        'action'        => 'index'
+                    )
+                )
+            ),
             'market' => array(
                 'type'    => 'Literal',
                 'options' => array(
                     'route'    => '/market',
                     'defaults' => array(
                         'controller'    => 'market-index-controller',
-                        'action'        => 'index',
-                    ),
+                        'action'        => 'index'
+                    )
                 ),
                 'may_terminate' => true,
                 'child_routes' => array(
-                    'index' => array(
-                        'type'    => 'Segment',
-                        'options' => array(
-                            'route'    => '/[:action]',
-                            'constraints' => array(
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
-                        ),
-                    ),
                     'view' => array(
-                        'type'    => 'Segment',
+                        'type' => 'Literal',
                         'options' => array(
-                            'route'    => '/[:action]',
-                            'constraints' => array(
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
+                            'route' => '/view',
+                            'defaults' => array(
+                                'controller' => 'market-view-controller',
+                                'action' => 'index',
+                            )
                         ),
+                        'may_terminate' => true,
+                        'child_routes' => array(
+                            'main' => array(
+                                'type' => 'Segment',
+                                'options' => array(
+                                    'route' => '/main[/:category]',
+                                    'defaults' => array(
+                                        'action'        => 'index'
+                                    )
+                                )
+                            ),
+                            'item' => array(
+                                'type' => 'Segment',
+                                'options' => array(
+                                    'route' => '/item[/:itemId]',
+                                    'defaults' => array(
+                                        'action'    => 'item'
+                                    ),
+                                    'constraints' => array(
+                                        'itemId' => '[0-9]*'
+                                    )
+                                )
+                            )
+                        )
                     ),
                     'post' => array(
-                        'type'    => 'Segment',
-                        'options' => array(
-                            'route'    => '/[:action]',
-                            'constraints' => array(
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
-                        ),
-                    ),
-                ),
-            ),
-            /*'home' => array(
-                'type'    => 'Literal',
-                'options' => array(
-                    'route'    => '/',
-                    'defaults' => array(
-                        'controller'    => 'market-index-controller',
-                        'action'        => 'index',
-                    ),
-                ),
-            ),
-            'market-view' => array(
-                'type'    => 'Literal',
-                'options' => array(
-                    'route'    => '/market/view',
-                    'defaults' => array(
-                        'controller'    => 'market-view-controller',
-                        'action'        => 'index',
-                    ),
-                ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                    'slash' => array(
                         'type'    => 'Literal',
                         'options' => array(
-                            'route'    => '/',
-                            'defaults' => array(
-                                'controller'    => 'market-view-controller',
-                                'action'        => 'index',
-                            ),
-                        ),
-                    ),
-                    'index' => array(
-                        'type' => 'Segment',
-                        'options' => array(
-                            'route' => '/main[/:category]',
-                        ),
-                    ),
-                    'item' => array(
-                        'type' => 'Segment',
-                        'options' => array(
-                            'route' => '/item[/:itemId]',
-                        ),
-                    ),
-                ),
-            ),
-            'market-post' => array(
-                'type'    => 'Literal',
-                'options' => array(
-                    'route'    => '/market/post',
-                    'defaults' => array(
-                        'controller'    => 'market-post-controller',
-                        'action'        => 'index',
-                    ),
-                ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                    'slash' => array(
-                        'type'    => 'Literal',
-                        'options' => array(
-                            'route'    => '/',
+                            'route'    => '/post',
                             'defaults' => array(
                                 'controller'    => 'market-post-controller',
-                                'action'        => 'index',
-                            ),
-                        ),
-                    ),
-                ),
-            ),
-            /*'market' => array(
-                'type'    => 'Literal',
-                'options' => array(
-                    // Change this to something specific to your module
-                    'route'    => '/market',
-                    'defaults' => array(
-                        // Change this value to reflect the namespace in which
-                        // the controllers for your module are found
-                        //'__NAMESPACE__' => 'Market\Controller',
-                        'controller'    => 'market-index-controller',
-                        'action'        => 'index',
-                    ),
-                ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                    // This route is a sane default when developing a module;
-                    // as you solidify the routes for your module, however,
-                    // you may want to remove it and replace it with more
-                    // specific routes.
-                    'default' => array(
-                        'type'    => 'Segment',
-                        'options' => array(
-                            'route'    => '/[:controller[/:action]]',
-                            'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
-                            'defaults' => array(
-                            ),
-                        ),
-                    ),
-                ),
-            ),*/
-        ),
+                                'action'        => 'index'
+                            )
+                        )
+                    )
+                )
+            )
+        )
     ),
     'view_manager' => array(
         'template_path_stack' => array(
