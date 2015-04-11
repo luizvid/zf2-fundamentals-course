51663d434595d763e266d4623c876c82e9ba2c39
Luiz Vid <luizvid@gmail.com>
2015-04-11 01:54:23 -0300

    M9Ex1E

--- module/Market/src/Market/Form/PostForm.php
+++ module/Market/src/Market/Form/PostForm.php
@@ -33,6 +33,37 @@ class PostForm extends Form
         $title->setLabel('Título');
         $title->setAttributes(array('maxlength' => 128, 'class' => 'form-control'));
 
+        $description = new Element\Textarea('description');
+        $description->setAttribute('class', 'form-control');
+        $description->setLabel('Descrição');
+
+        $photoFilename = new Element\File('photo_filename');
+        $photoFilename->setLabel('Foto');
+
+        $contactName = new Element\Text('contact_name');
+        $contactName->setAttribute('class', 'form-control');
+        $contactName->setLabel('Nome completo');
+
+        $contactEmail = new Element\Email('contact_email');
+        $contactEmail->setAttribute('class', 'form-control');
+        $contactEmail->setLabel('E-mail');
+
+        $contactPhone = new Element\Text('contact_phone');
+        $contactPhone->setAttribute('class', 'form-control');
+        $contactPhone->setLabel('Telefone');
+
+        $city = new Element\Text('city');
+        $city->setAttribute('class', 'form-control');
+        $city->setLabel('Cidade');
+
+        $country = new Element\Text('country');
+        $country->setAttributes(array('class' => 'form-control', 'maxlength' => 2));
+        $country->setLabel('País');
+
+        $price = new Element\Number('price');
+        $price->setAttributes(array('class' => 'form-control', 'maxlength' => 12, 'min' => 0, 'max' => '999999999999'));
+        $price->setLabel('Preço');
+
         $csrf = new Element\Csrf('security');
 
         $submit = new Element\Submit('submit');
@@ -41,7 +72,15 @@ class PostForm extends Form
         //$form = new Form('contact');
         $this->add($category)
             ->add($title)
+            ->add($description)
+            ->add($photoFilename)
+            ->add($contactName)
+            ->add($contactEmail)
+            ->add($contactPhone)
+            ->add($city)
+            ->add($country)
             ->add($csrf)
+            ->add($price)
             ->add($submit);
 
 
