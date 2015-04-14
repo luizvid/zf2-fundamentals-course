fa1e79dc7874ca548379b0bc06cf0cfaae7c44ef
Luiz Vid <luizvid@gmail.com>
2015-04-13 22:41:23 -0300

    autoload_classmap atualizados

--- module/Application/autoload_classmap.php
+++ module/Application/autoload_classmap.php
@@ -3,6 +3,7 @@
 return array(
     'Application\Module'                     => __DIR__ . '/Module.php',
     'Application\Controller\IndexController' => __DIR__ . '/src/Application/Controller/IndexController.php',
+    'Application\Helper\LeftLinks'           => __DIR__ . '/src/Application/Helper/LeftLinks.php',
     'Application\Service\ExemploService'     => __DIR__ . '/src/Application/Service/ExemploService.php',
     'Application\Service\SampleService'      => __DIR__ . '/src/Application/Service/SampleService.php',
 );
